d0496bcc7d8aad67f42fbd76d747a8d003809e90
Izzudien Al Faqih <izzusnbp@gmail.com>
2026-01-11 09:47:57 +0700

    tampilan

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, contact
+from .views import home, contact, gallery
 
 urlpatterns = [
     path("", home, name="home"),
+    path("gallery/", gallery, name="gallery"),
     path("contact/", contact, name="contact"),
+    
 ]
